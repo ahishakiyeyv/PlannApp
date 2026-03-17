63441d8ae582ecdcc0a2e2d82b2abedaf43c41fc
Ahishakiyeyv <yvanahishakiye6@gmail.com>
2026-03-17 19:46:51 +0100

    container added

--- lib/features/assignment/assignment_screen.dart
+++ lib/features/assignment/assignment_screen.dart
@@ -34,8 +34,43 @@ class AssignmentScreen extends StatelessWidget {
             ],),
           ),
         ),
-        body: const TabBarView(children: [
-          Center(child: Text('All Assignments')),
+        body: TabBarView(children: [
+          Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 16.0, bottom: 16.0, right: 8.0, left: 8.0),
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1.0, color: Colors.grey),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: 
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Mobile App Project'),
+                          Text('UI Design'),
+                          Text('Priority: High'),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text('Pending'),
+                          Text('Due: 2024-06-30'),
+                          Text('High'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
           Center(child: Text('Pending Assignments')),
           Center(child: Text('Completed Assignments')),
         ],
