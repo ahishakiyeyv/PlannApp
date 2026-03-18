2ab7648d7776e216cca262da96a9affe14a4cf41
Ahishakiyeyv <yvanahishakiye6@gmail.com>
2026-03-18 19:28:29 +0100

    assignments content added

--- lib/features/assignment/assignment_screen.dart
+++ lib/features/assignment/assignment_screen.dart
@@ -38,7 +38,7 @@ class AssignmentScreen extends StatelessWidget {
           Column(
             children: [
               Container(
-                margin: EdgeInsets.only(top: 16.0, bottom: 16.0, right: 8.0, left: 8.0),
+                margin: EdgeInsets.only(top: 32.0, bottom: 4.0, right: 8.0, left: 8.0),
                 decoration: BoxDecoration(
                   border: Border.all(width: 1.0, color: Colors.grey),
                   borderRadius: BorderRadius.circular(8.0),
@@ -52,17 +52,231 @@ class AssignmentScreen extends StatelessWidget {
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text('Mobile App Project'),
-                          Text('UI Design'),
+                          Text('Mobile App Project', style: TextStyle(fontWeight: FontWeight.w700,fontSize: 20.0),),
+                          Text('UI Design', style: TextStyle(fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),),
                           Text('Priority: High'),
                         ],
                       ),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
-                          Text('Pending'),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                              child: Text('Pending', style: TextStyle(color: Colors.white)),
+                            ),
+                          ),
                           Text('Due: 2024-06-30'),
-                          Text('High'),
+                          
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 8.0, bottom: 4.0, right: 8.0, left: 8.0),
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1.0, color: Colors.grey),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: 
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Database Project', style: TextStyle(fontWeight: FontWeight.w700,fontSize: 20.0),),
+                          Text('SQL', style: TextStyle(fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),),
+                          Text('Priority: medium'),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.yellow[700],
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                              child: Text('Pending', style: TextStyle(color: Colors.white)),
+                            ),
+                          ),
+                          Text('Due: 2024-06-30'),
+                          
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 8.0, bottom: 4.0, right: 8.0, left: 8.0),
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1.0, color: Colors.grey),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: 
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Networking Report', style: TextStyle(fontWeight: FontWeight.w700,fontSize: 20.0),),
+                          Text('Computer networks', style: TextStyle(fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),),
+                          Text('Priority: low'),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.green[700],
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                              child: Text('Pending', style: TextStyle(color: Colors.white)),
+                            ),
+                          ),
+                          Text('Due: 2024-06-30'),
+                          
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 8.0, bottom: 4.0, right: 8.0, left: 8.0),
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1.0, color: Colors.grey),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: 
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Wireframing Project', style: TextStyle(fontWeight: FontWeight.w700,fontSize: 20.0),),
+                          Text('UI Design', style: TextStyle(fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),),
+                          Text('Priority: Medium'),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.yellow[700],
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                              child: Text('Pending', style: TextStyle(color: Colors.white)),
+                            ),
+                          ),
+                          Text('Due: 2024-06-30'),
+                          
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 8.0, bottom: 4.0, right: 8.0, left: 8.0),
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1.0, color: Colors.grey),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: 
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Prototyping Project', style: TextStyle(fontWeight: FontWeight.w700,fontSize: 20.0),),
+                          Text('High Fidelity', style: TextStyle(fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),),
+                          Text('Priority: High'),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                              child: Text('Pending', style: TextStyle(color: Colors.white)),
+                            ),
+                          ),
+                          Text('Due: 2024-06-30'),
+                          
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 8.0, bottom: 4.0, right: 8.0, left: 8.0),
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1.0, color: Colors.grey),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: 
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Mathematics Project', style: TextStyle(fontWeight: FontWeight.w700,fontSize: 20.0),),
+                          Text('Algebra', style: TextStyle(fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),),
+                          Text('Priority: Low'),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.green[700],
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                              child: Text('Pending', style: TextStyle(color: Colors.white)),
+                            ),
+                          ),
+                          Text('Due: 2024-06-30'),
+                          
                         ],
                       ),
                     ],
